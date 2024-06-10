c83d4fa280c2bd5e6365d82d139a757a3bcb26e8
Pausha B Arias <sle535567@gmail.com>
2024-06-10 10:36:15 -0700

    Add files via upload

--- api/users/router.js
+++ api/users/router.js
@@ -68,11 +68,15 @@ router.delete('/:id', async (req, res, next) => {
 })
 
 // ERRORS
-router.use((err, req, res, next) => { // eslint-disable-line
-	res.status(err.status || 500).json({
-		message: err.message,
-		stack: err.stack,
-	})
-})
+router.use((err, req, res, next) => {
+    if (res.headersSent) {
+        return next(err);
+    }
+    
+    res.status(err.status || 500).json({
+        message: err.message,
+        stack: err.stack,
+    });
+});
 
 module.exports = router
